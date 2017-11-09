4aafc921691bd1b2fce71800a4e5d405f3200815
Joseph Dalmasso <me@jodalmasso.com>
2017-11-09 15:37:22 -0500

    Added Home component

---
+++ imports/ui/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <img src="/images/grumpy_Cat.jpg" alt="" />
+        </div>
+    );
+}
+
+export default Home;
