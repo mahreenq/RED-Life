87ad5a9a80a2fd3cde257ec01b8353a62f7a12bc
Mahreen Qureshi <mahreenq@hotmail.com>
2017-11-09 13:31:13 -0500

    combining collection array

--- imports/ui/containers/ProfileContainer/ProfileContainer.js
+++ imports/ui/containers/ProfileContainer/ProfileContainer.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
+
 import { Profiles } from '../../../collections/profiles';
+import { Ideas } from '../../../collections/ideas';
+
 import Profile from './Profile';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 
 
 
+
 class ProfileContainer extends Component {
 
   render() {
-    console.log('userId', this.props.profiles);
+     console.log(this.props.ideas);
+    // console.log(this.props.profiles);
+
       let usersData = this.props.profiles;
+      let ideasData = this.props.ideas ? this.props.ideas : null;
+
+
+
+      
       let userid = this.props.match.params.userid;
+
+
+
+
+      let dataArray = ideasData.map(idea =>{
+        const newIdeaVotes = usersData.find( (user)=> idea.votes === user._id)
+        idea.votes = newIdeaVotes;
+  
+        return idea;
+      })
+
+      console.log(dataArray);
+
+
+
+
+
+
+
+
+
+
 
       let profileData = usersData.filter((user) => {
         return (
@@ -20,8 +53,15 @@ class ProfileContainer extends Component {
         );
       })
 
+      // let voteData = ideasData.map((user) => {
+      //   return (
+      //     userid === user._id
+      //   );
+      // })
+
+
       return (
-          <Profile profileData={profileData}  />
+          <Profile profileData={profileData} ideasData={ideasData}  />
           
       );
   }
@@ -31,9 +71,10 @@ class ProfileContainer extends Component {
 
 export default createContainer(() => {
   //setup subscription, pass in the publications name
-  Meteor.subscribe('users'); //Whatever is available from the publication will be returned here
+  Meteor.subscribe('usersAndIdeas'); //Whatever is available from the publication will be returned here
   //return an object, whatever that is returned will be available on props for this component
   return {
-    profiles: Profiles.find({}).fetch()
+    profiles: Profiles.find({}).fetch(),
+    ideas: Ideas.find({}).fetch()
   }; //We need to call fetch() that will invoke the cursor to actually execute the query
 }, ProfileContainer);
