f461a53be80d03c59b6e5626296dac7579f7f1bc
Joseph Dalmasso <me@jodalmasso.com>
2017-11-14 11:58:53 -0500

    Changed login

---
+++ imports/ui/components/forms/Register.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import EmailPasswordForm from './EmailPasswordForm';
+
+class Register extends Component{
+    getDefaultProps() {
+      let loginMsg = "Already have an account?";
+      return {
+        loginLink: <p>{loginMsg} <a href="/login">Sign In</a></p>
+      };
+    }
+
+    createUser(e) {
+      e.preventDefault();
+      const
+        email = $('#email').val(),
+        password = $('#password').val().trim()
+      ;
+  
+      Accounts.createUser(
+        {
+          email: email,
+          password: password
+        },
+        function(error) {
+          if (error) {
+            console.log("there was an error: " + error.reason);
+          } else { 
+            FlowRouter.go('home');
+          };
+        }
+      );
+    }
+
+    render() {
+      return (
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3">
+            <h1>Register</h1>
+              <EmailPasswordForm
+                submitBtnLabel="Register"
+                submitAction={this.createUser}
+              />
+             {this.props.loginLink}
+          </div>
+        </div>
+      )
+    }
+};
+
+export default Register;
