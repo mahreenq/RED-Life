b3955230d396a9d2fdac9c9acdadd22d70b5d6d0
erxgithub <erxlink@gmail.com>
2017-11-14 11:59:57 -0500

    Added styling for home page.

--- imports/ui/components/Home/Home.js
+++ imports/ui/components/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './styles.css';
 
 const Home = () => {
     return (
-        <div>
-            <img src="/images/grumpy_Cat.jpg" alt="" />
+        <div className="homeContainer">
+            <div className="homeSection">
+                <h1>RED <span>Life</span></h1>
+            </div>
+            <div className="loginSection">
+            </div>
         </div>
     );
 }
