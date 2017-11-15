89435878c3b004ad5cc837c0557ebcbe18ce0a9c
Joseph Dalmasso <me@jodalmasso.com>
2017-11-15 17:03:35 -0500

    Fixed red button

--- imports/ui/components/Layout/Header/Header.js
+++ imports/ui/components/Layout/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import {Link} from 'react-router-dom';
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
@@ -20,17 +20,14 @@ const styles = {
     }
 };
 
-
-const Header = () => (
-    
-    <div className="Header">
-        
+const RenderWithLinkToLogin = () => {
+    return (
+        <div className="Header">
         <Toolbar style={styles.header}>
             <ToolbarGroup firstChild={true}>
-            {location.pathname !== '/login' && location.pathname !== '/setupprofile' && location.pathname !== '/register' && location.pathname !== '/'?
-                <Link to="/ideas">
-                    <img src="/images/red-logo.png" alt="red logo" className="logo" />
-                </Link> : <img src="/images/red-logo.png" alt="red logo" className="logo" />}
+            <Link to="/">
+                <img src="/images/red-logo.png" alt="red logo" className="logo" />
+            </Link>
                 <ToolbarTitle text="Life" style={styles.title} />
             </ToolbarGroup>
             <ToolbarGroup lastChild={true}>
@@ -38,6 +35,60 @@ const Header = () => (
             </ToolbarGroup>
         </Toolbar>
     </div>
-);
+    )
+}
+
+const RenderWithLinkIdeas = () => {
+    return (
+        <div className="Header">
+        <Toolbar style={styles.header}>
+            <ToolbarGroup firstChild={true}>
+            <Link to="/ideas">
+                <img src="/images/red-logo.png" alt="red logo" className="logo" />
+            </Link>
+                <ToolbarTitle text="Life" style={styles.title} />
+            </ToolbarGroup>
+            <ToolbarGroup lastChild={true}>
+            {location.pathname !== '/login' && location.pathname !== '/setupprofile' ? <Button /> : null}
+            </ToolbarGroup>
+        </Toolbar>
+        </div>
+    )
+} 
+
+const RenderWithJustImage = () => {
+    return (
+        <div className="Header">
+        <Toolbar style={styles.header}>
+            <ToolbarGroup firstChild={true}>
+                <img src="/images/red-logo.png" alt="red logo" className="logo" />
+                <ToolbarTitle text="Life" style={styles.title} />
+            </ToolbarGroup>
+            <ToolbarGroup lastChild={true}>
+            {location.pathname !== '/login' && location.pathname !== '/setupprofile' ? <Button /> : null}
+            </ToolbarGroup>
+        </Toolbar>
+    </div>
+    )
+}
+
+class Header extends Component {
+
+    render() {
+        let logo = null;
+        if (location.pathname !== '/login' && location.pathname !== '/setupprofile' && location.pathname !== '/') {
+            return logo = <RenderWithLinkToLogin />
+        }else if (location.pathname !== '/register'){
+            return logo = <RenderWithLinkIdeas />
+        }else {
+            return logo = <RenderWithJustImage />
+        }
+
+
+    return (
+        {logo}
+    )
+};
+}
 
 export default Header;
