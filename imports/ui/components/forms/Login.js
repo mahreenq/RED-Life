ed2d81930948058827d989f9da676d26bc6d0671
Joseph Dalmasso <me@jodalmasso.com>
2017-11-15 16:05:02 -0500

    Protected routes

--- imports/ui/components/forms/Login.js
+++ imports/ui/components/forms/Login.js
@@ -5,43 +5,19 @@ import EmailPasswordForm from './EmailPasswordForm'
 
 class Login extends Component{
     loginWithPassword = (e) => {
-        e.preventDefault();
-
-        const
-            email = $('#email').val(),
+      e.preventDefault();
+      const email = $('#email').val(),
             password = $('#password').val().trim()
-        ;
+      ;
 
-        // form field validations
-
-        let errorMessage = "";
-        let fieldLength = 0;
-
-        fieldLength = email.length;
-        if (fieldLength === 0) {
-            errorMessage += "Email cannot be blank.\n"
-        } else if (fieldLength > 30) {
-            errorMessage += "Email length cannot exceed 50 characters.\n"
-        }
-
-        fieldLength = password.length;
-        if (fieldLength === 0) {
-            errorMessage += "Password cannot be blank.\n"
-        }
-
-        if (errorMessage.length > 0) {
-            errorMessage += "\nPlease correct before submitting.\n"
-            alert(errorMessage);
+      Meteor.loginWithPassword(email, password, function(error) {
+        if (error) {
+          console.log("There was an error:" + error.reason);
         } else {
-            Meteor.loginWithPassword(email, password, function(error) {
-                if (error) {
-                    console.log("There was an error:" + error.reason);
-                } else {
-                    FlowRouter.go('/');
-                }
-            });
-            this.props.history.push(`/ideas`);
+          FlowRouter.go('/');
         }
+      });
+      this.props.history.push(`/ideas`);
     }
 
     render() {
